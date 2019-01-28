252b73debf6a0afe72000f7a7e5be6d8d787e227
李盛美 <mino_lee@163.com>
2019-01-28 20:53:12 +0800

    player:get music url finish

--- src/api/singer.js
+++ src/api/singer.js
@@ -51,3 +51,42 @@ export function getSingerDetail (mid) {
     return Promise.resolve(res.data)
   })
 }
+
+export function getSongVkey (songid) {
+  let url = '/getSongVkey'
+
+  const data = Object.assign({}, commonParams, {
+    loginUin: 0,
+    hostUin: 0,
+    format: 'json',
+    platform: 'yqq.json',
+    needNewCode: 0
+  }, {
+    data: {
+      req: {
+        module: 'CDN.SrfCdnDispatchServer',
+        method: 'GetCdnDispatch',
+        param: {'guid': '4746141188', 'calltype': 0, 'userip': ''}
+      },
+      req_0: {
+        module: 'vkey.GetVkeyServer',
+        method: 'CgiGetVkey',
+        param: {
+          guid: '4746141188',
+          songmid: [songid],
+          songtype: [0],
+          uin: '0',
+          loginflag: 1,
+          platform: '20'
+        }
+      },
+      comm: {uin: 0, format: 'json', ct: 24, cv: 0}
+    }
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,15 @@
+import {playMode} from '../common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,  //正在播放
+  fullScreen: false,
+  // 顺序播放时，playList和sequenceList一致，其余时不一致
+  playlist: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  // 当前播放的歌曲
+  currentIndex: -1
 }
 
 export default state
